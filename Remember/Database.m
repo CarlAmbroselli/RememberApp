9186b08e3d103679819f3a83c314e7ae11ccd6a3
Markus Petrykowski <M.Petrykowski@gmx.de>
2013-11-03 14:02:40 +0100

    Data added

--- Remember/Database.m
+++ Remember/Database.m
@@ -140,13 +140,113 @@
                               @"locationLong" : @93.33223f,
                               @"headline" : @"Pokemon Fight Sound"};
     
-    NSDictionary *mediaOneDict = @{@"fileName": @"pokemonBattleSound.pm3",
+    NSDictionary *mediaOneDict = @{@"fileName": @"pokemonBattleSound.mp3",
                                @"mediaId" : @1 };
     
     Post *postOne = [Post postWithDict:postOneDict inManagedObjectContext:[self managedObjectContext]];
     Media *mediaOne = [Media mediaWithDict:mediaOneDict inManagedObjectContext:[self managedObjectContext]];
     
     [postOne addMediaObject:mediaOne];
+    
+    NSDictionary *postTwoDict = @{@"postId": @2,
+                                  @"range" : @"everyone",
+                                  @"year" : @"1990's",
+                                  @"isLiked" : @NO,
+                                  @"type" : @"video",
+                                  @"locationLat" : @0.0f,
+                                  @"locationLong" : @0.0f,
+                                  @"headline" : @"The Firestone Family"};
+    
+    NSDictionary *mediaTwoDict = @{@"fileName": @"firestone_intro_80er_90er.mp4",
+                                   @"mediaId" : @1 };
+    
+    Post *postTwo = [Post postWithDict:postTwoDict inManagedObjectContext:[self managedObjectContext]];
+    Media *mediaTwo = [Media mediaWithDict:mediaTwoDict inManagedObjectContext:[self managedObjectContext]];
+    
+    [postTwo addMediaObject:mediaTwo];
+    
+    
+    NSDictionary *postThreeDict = @{@"postId": @3,
+                                  @"range" : @"everyone",
+                                  @"year" : @"1998",
+                                  @"isLiked" : @NO,
+                                  @"type" : @"image",
+                                  @"locationLat" : @0.0f,
+                                  @"locationLong" : @0.0f,
+                                  @"headline" : @"Furby",
+                                };
+    
+    NSDictionary *mediaThreeDict = @{@"fileName": @"furby_1998.jpg",
+                                   @"mediaId" : @3 };
+    
+    Post *postThree = [Post postWithDict:postThreeDict inManagedObjectContext:[self managedObjectContext]];
+    Media *mediaThree = [Media mediaWithDict:mediaThreeDict inManagedObjectContext:[self managedObjectContext]];
+    
+    [postThree addMediaObject:mediaThree];
+    
+    NSDictionary *post4Dict = @{@"postId": @4,
+                                    @"range" : @"everyone",
+                                    @"year" : @"1984",
+                                    @"isLiked" : @NO,
+                                    @"type" : @"sound",
+                                    @"locationLat" : @0.0f,
+                                    @"locationLong" : @0.0f,
+                                    @"headline" : @"Tetris",
+                                    };
+    
+    NSDictionary *media4Dict = @{@"fileName": @"tetris_Gameboy.jpg",
+                                     @"mediaId" : @4 };
+    
+    NSDictionary *media4Dict2 = @{@"fileName": @"original_tetris.jpg",
+                                 @"mediaId" : @42 };
+    
+    NSDictionary *media4Dict3 = @{@"fileName": @"tetris_sound_1984.mp3",
+                                 @"mediaId" : @43 };
+    
+    Post *post4 = [Post postWithDict:post4Dict inManagedObjectContext:[self managedObjectContext]];
+    Media *media4 = [Media mediaWithDict:media4Dict inManagedObjectContext:[self managedObjectContext]];
+    Media *media42 = [Media mediaWithDict:media4Dict2 inManagedObjectContext:[self managedObjectContext]];
+    Media *media43 = [Media mediaWithDict:media4Dict3 inManagedObjectContext:[self managedObjectContext]];
+    
+    [post4 addMediaObject:media4];
+    [post4 addMediaObject:media42];
+    [post4 addMediaObject:media43];
+    
+    NSDictionary *post5Dict = @{@"postId": @5,
+                                  @"range" : @"everyone",
+                                  @"year" : @"1996",
+                                  @"isLiked" : @NO,
+                                  @"type" : @"sound",
+                                  @"locationLat" : @0.0f,
+                                  @"locationLong" : @0.0f,
+                                  @"headline" : @"YU-GI-OH",
+                                @"contentText" : @"The legendary blue eyes white dragon"};
+    
+    NSDictionary *media5Dict = @{@"fileName": @"blue_eyes_white_dragon_1996.jpg",
+                                   @"mediaId" : @5 };
+    
+    Post *post5 = [Post postWithDict:post5Dict inManagedObjectContext:[self managedObjectContext]];
+    Media *media5 = [Media mediaWithDict:media5Dict inManagedObjectContext:[self managedObjectContext]];
+    
+    [post5 addMediaObject:media5];
+
+    NSDictionary *post6Dict = @{@"postId": @6,
+                                @"range" : @"everyone",
+                                @"year" : @"1987",
+                                @"isLiked" : @NO,
+                                @"type" : @"sound",
+                                @"locationLat" : @0.0f,
+                                @"locationLong" : @0.0f,
+                                @"headline" : @"Kraftwerk - Taschenrechner"};
+    
+    NSDictionary *media6Dict = @{@"fileName": @"Kraftwerk_Taschenrechner_1987.mp3",
+                                 @"mediaId" : @6 };
+    
+    Post *post6 = [Post postWithDict:post6Dict inManagedObjectContext:[self managedObjectContext]];
+    Media *media6 = [Media mediaWithDict:media6Dict inManagedObjectContext:[self managedObjectContext]];
+    
+    [post6 addMediaObject:media6];
+
     
 }
 
